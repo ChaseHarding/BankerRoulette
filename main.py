1773ac2780b5cff2d1cfafa737b1641268416b68
ChaseHarding <chaseharding965@gmail.com>
2024-07-31 17:46:41 -0700

    takes in a number of participants, takes in names, then prints a random name from the list

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ def banker_roulette():
     # create empty list to store names
     names = []
 
+    # collect names from user
     for _ in range(num_participants):
         name = input("Enter a name: ")
         names.append(name)
+
+    #randomly pick a name from the list
+    chosen_one = names[random.randint(0, len(names) -1)]
+
+    print(f"The chosen one is {chosen_one}!")
+
+banker_roulette()
